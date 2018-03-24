25640991b6130066ec39393da2ae7d7cc35ec48b
Arno <arno.galvez@gmail.com>
2018-03-25 00:44:00 +0100

    PHILLLLLLLIIIPPPPP

--- quiz.py
+++ quiz.py
@@ -2,45 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_string("""
-<MenuScreen>:
-    canvas.before:
-        BorderImage:
-            border: 10, 10, 10, 10
-            source: 'doom.jpg'
-            pos: self.pos
-            size: self.size
-    FloatLayout:
-        Button:
-            size_hint: None, None
-            text: 'Start'
-            pos:100, 75
-            width: '250sp'
-            font_size: '40sp'
-            on_press: 
-                root.manager.transition.direction = 'left'
-                root.manager.current = 'game'
-        Button:
-            size_hint: None, None
-            pos:450, 75
-            width: '250sp'
-            font_size: '40sp'
-            text: 'Quit'
-            on_press: root.dismiss()
-
-<SettingsScreen>:
-    FloatLayout:  
-        Button:
-            size_hint: None, None
-            text: 'Back to menu'
-            pos:520, 475
-            width: '250sp'
-            font_size: '20sp'
-            on_press: 
-                root.manager.transition.direction = 'right'
-                root.manager.current = 'menu'
-
-""")
+Builder.load_file("quiz2.kv")
 
 class MenuScreen(Screen):
     pass
